c25d34459734cd17501e9dae98286257e232c649
Sam Wu <sjwu@ualberta.ca>
2021-07-19 21:08:12 -0600

    add bg images

--- js/game.js
+++ js/game.js
@@ -13,12 +13,22 @@ var game = new Phaser.Game(config);
 
 function preload () {
     this.load.image('trex', 'assets/images/clicker/trex.png');
+
+    this.load.image('forest-front', 'assets/images/backgrounds/parallax/battleback1-1.png');
+    this.load.image('forest-back', 'assets/images/backgrounds/parallax/battleback1-2.png');
 }
 
 var trexClicker;
 function create () {
-    trexClicker = this.add.sprite(450, 290, 'trex');
+    trexClicker = this.add.sprite(500, 290, 'trex');
     trexClicker.setOrigin(0.5, 0.5);
+
+    background = this.add.group();
+    backgroundImages = ['forest-front', 'forest-back'];
+    // backgroundImages.forEach(function (image) {
+    //     var bg = this.scene.add.tileSprite(0, 0, this.world.width, this.world.height, image, '', background);
+    //     bg.tileScale.setTo(4,4);
+    // });
 }
 
 function update () {
